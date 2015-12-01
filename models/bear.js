7fb32f32829913abc3f7fcbd37b81a367ff0beda
Kristian <kristiantran@gmail.com>
2015-12-01 14:42:22 -0500

    Initial Commit
    Demonstrating REST API principles with Express.js, MongoDB, and Node.js

---
+++ models/bear.js
@@ -0,0 +1,10 @@
+// bearApi/models/bear.js
+
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var BearSchema   = new Schema({
+    name: String
+});
+
+module.exports = mongoose.model('Bear', BearSchema);
